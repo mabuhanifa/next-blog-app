42007e4a996fb26f76e5416bf84fc33f2995c5ef
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-09 02:27:14 +0600

    getServerSideProps category

--- pages/news/[category].js
+++ pages/news/[category].js
@@ -1,7 +1,29 @@
-import React from 'react'
+import React from "react";
 
-export default function ArticleListByCategory() {
+export default function ArticleListByCategory({ articles, category }) {
   return (
-    <div>Category</div>
-  )
+    <div>
+      <h2>Category {category}</h2>
+      <div>
+        {articles.map((articles) => (
+          <div key={articles.id}>
+            <h4>{articles.title}</h4>
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+}
+
+export async function getServerSideProps({ params: { category } }) {
+  const response = await fetch(
+    `http://localhost:3004/news?category=${category}`
+  );
+  const data = await response.json();
+  return {
+    props: {
+      articles: data,
+      category,
+    },
+  };
 }
