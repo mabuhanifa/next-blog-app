6d0b4a7a56eba9fc357de7bea463610acdad8fda
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-09 20:18:33 +0600

    push

--- pages/events.js
+++ pages/events.js
@@ -1,11 +1,14 @@
+import { useRouter } from "next/router";
 import React, { useState } from "react";
 
 export default function EventList({ eventList }) {
+  const { push } = useRouter();
   const [events, setEvents] = useState(eventList);
   const fetchEvents = async () => {
     const res = await fetch("http://localhost:3004/events?category=sports");
     const data = await res.json();
     setEvents(data);
+    push("/events?category=sports", undefined, { shallow: true });
   };
   return (
     <div>
@@ -25,7 +28,7 @@ export default function EventList({ eventList }) {
 
 export async function getServerSideProps({ query: { category } }) {
   const queryString = category ? "category" : "";
-  const res = await fetch("http://localhost:3004/events");
+  const res = await fetch(`http://localhost:3004/events?${queryString}`);
   const data = await res.json();
   return {
     props: {
