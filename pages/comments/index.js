1793bf5a3662a213d39cca503c629d37897d69f0
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-10 23:52:14 +0600

    state

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -1,7 +1,12 @@
-import React from 'react'
+import React from "react";
 
 export default function CommentsPage() {
+  const [comments, setComments] = useState([]);
+  
   return (
-    <div>CommentsPage</div>
-  )
+    <div>
+      <button onClick={fetchComments}>Load Comments</button>
+      
+    </div>
+  );
 }
