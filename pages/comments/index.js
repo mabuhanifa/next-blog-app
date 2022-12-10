d9492170ea78390c864d77febd1c61be3a5bbbee
shourovahmd <moh.abuhanifa@gmail.com>
2022-12-10 23:54:35 +0600

    fetchComments

--- pages/comments/index.js
+++ pages/comments/index.js
@@ -2,7 +2,11 @@ import React from "react";
 
 export default function CommentsPage() {
   const [comments, setComments] = useState([]);
-  
+  const fetchComments = async () => {
+    const res = await fetch("http://localhost:3004/api/comments");
+    const data = await res.json();
+    setComments(data);
+  };
   return (
     <div>
       <button onClick={fetchComments}>Load Comments</button>
