2cedb486e3955404d96d226ab448c5355b7c7559
mabuhanifa <moh.abuhanifa@gmail.com>
2025-04-08 22:14:17 +0600

    Slider added

--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,19 @@
 import Component from "@/components/Carousel";
+import { InfiniteSliderHoverSpeed } from "@/components/InfiniteSliderHoverSpeed";
+import { InfiniteSliderVertical } from "@/components/InfiniteSliderVertical";
 
 export default function Home() {
   return (
-    <div className="flex justify-center bg-gray-100">
-      <Component />
-    </div>
+    <>
+      <div className="flex justify-center bg-gray-100">
+        <Component />
+      </div>
+      <div className="my-10 mx-5 lg:mx-20 2xl:mx-80">
+        <InfiniteSliderHoverSpeed />
+      </div>
+      <div className="my-10 mx-5 lg:mx-20 2xl:mx-80 flex justify-center">
+        <InfiniteSliderVertical />
+      </div>
+    </>
   );
 }
---
+++ components/InfiniteSliderHoverSpeed.tsx
@@ -0,0 +1,68 @@
+import { InfiniteSlider } from "./motion-primitives/infinite-slider";
+
+export function InfiniteSliderHoverSpeed() {
+  return (
+    <InfiniteSlider speedOnHover={50} gap={24}>
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02ad24c5e36ddcd1957ad35677"
+        alt="Dean blunt - Black Metal 2"
+        className="aspect-square object-cover w-[120px] rounded-[4px] "
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02af73f776b92d4614152fb141"
+        alt="Jungle Jack - JUNGLE DES ILLUSIONS VOL 2"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02ecdb8f824367a53468100faf"
+        alt="Yung Lean - Stardust"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e021624590458126fc8b8c64c2f"
+        alt="Lana Del Rey - Ultraviolence"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e020dcf0f3680cff56fe5ff2288"
+        alt="A$AP Rocky - Tailor Swif"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02bc1028b7e9cd2b17c770a520"
+        alt="Midnight Miami (feat Konvy) - Nino Paid, Konvy"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.ibb.co.com/mFzJc5gb/455450613-7715052418604239-5080913386502490905-n.jpg"
+        alt="Dean blunt - Black Metal 2"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02af73f776b92d4614152fb141"
+        alt="Jungle Jack - JUNGLE DES ILLUSIONS VOL 2"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02ecdb8f824367a53468100faf"
+        alt="Yung Lean - Stardust"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e021624590458126fc8b8c64c2f"
+        alt="Lana Del Rey - Ultraviolence"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e020dcf0f3680cff56fe5ff2288"
+        alt="A$AP Rocky - Tailor Swif"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+      <img
+        src="https://i.scdn.co/image/ab67616d00001e02bc1028b7e9cd2b17c770a520"
+        alt="Midnight Miami (feat Konvy) - Nino Paid, Konvy"
+        className="aspect-square object-cover w-[120px] rounded-[4px]"
+      />
+    </InfiniteSlider>
+  );
+}
